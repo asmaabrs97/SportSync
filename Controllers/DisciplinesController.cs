0ef82491afd1b64be286ab7c1d2fbee1e74332d1
asmaabrs97 <asmaabrs9@gmail.com>
2024-12-16 04:33:28 +0100

    feat: Add individual sport discipline views and session details page

--- Controllers/DisciplinesController.cs
+++ Controllers/DisciplinesController.cs
@@ -5,10 +5,61 @@ namespace MyApp.Namespace
     public class DisciplinesController : Controller
     {
         // GET: DisciplinesController
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
 
+        public IActionResult SkiAlpin()
+        {
+            return View();
+        }
+
+        public IActionResult Football()
+        {
+            return View();
+        }
+
+        public IActionResult Basketball()
+        {
+            return View();
+        }
+
+        public IActionResult Handball()
+        {
+            return View();
+        }
+
+        public IActionResult Volleyball()
+        {
+            return View();
+        }
+
+        public IActionResult Baseball()
+        {
+            return View();
+        }
+
+        public IActionResult Cricket()
+        {
+            return View();
+        }
+
+        public IActionResult WaterPolo()
+        {
+            return View();
+        }
+
+        public IActionResult Details(string id)
+        {
+            return View();
+        }
+
+        public IActionResult SessionDetails(string discipline, string session)
+        {
+            ViewData["Discipline"] = discipline;
+            ViewData["Session"] = session;
+            return View();
+        }
     }
 }
